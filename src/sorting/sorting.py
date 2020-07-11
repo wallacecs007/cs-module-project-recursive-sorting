c1998028ee06894cfa75e0112a3e7015bf26baa8
Chase Wallace <wallacecs007@gmail.com>
2020-07-11 13:05:02 -0600

    Sorting finished

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -4,25 +4,46 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
+    i = 0
 
+    while len(arrA) > 0 and len(arrB) > 0:
+        if arrA[0] < arrB[0]:
+            merged_arr[i] = arrA[0]
+            del arrA[0]
+        else:
+            merged_arr[i] = arrB[0]
+            del arrB[0]
+        i += 1
+
+    for a in arrA or arrB:
+        merged_arr[i] = a
+        i += 1
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
+
+
 def merge_sort(arr):
     # Your code here
+    if len(arr) > 1:
+        mid = len(arr)//2
+        arrA = merge_sort(arr[:mid])
+        arrB = merge_sort(arr[mid:])
 
+        return merge(arrA, arrB)
 
     return arr
 
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
+# STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
+# In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
 
 
-def merge_sort_in_place(arr, l, r):
-    # Your code here
+# def merge_in_place(arr, start, mid, end):
+#     # Your code here
 
+
+# def merge_sort_in_place(arr, l, r):
+#     # Your code here
